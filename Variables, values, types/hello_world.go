c13c5c41a951927f22f7fdfaf2e48e47e6e0db4d
Avanish Ghosh <avanishghosh@gmail.com>
2021-06-07 12:22:08 +0530

    topic-wise

--- Variables, values, types/hello_world.go
+++ Variables, values, types/hello_world.go
@@ -5,8 +5,12 @@ import (
 )
 
 func main() {
-	fmt.Print("Hello World")
-	for i:= 0; i<100; i++ {
-		fmt.Println(i)
-	}
+	fmt.Println("Hello World")
+	n, _ := fmt.Println("", 2)
+	fmt.Print(n)
+
+	// for i := 0; i < 100; i++ {
+	// 	fmt.Println(i)
+
+	// }
 }
