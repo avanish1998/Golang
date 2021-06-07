9288cf812c7a9d1709418cc24f245b1c721adae4
Avanish Ghosh <avanishghosh@gmail.com>
2021-06-07 12:19:32 +0530

    topic-wise

---
+++ hello_world.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello World")
+	n, _ := fmt.Println("", 2)
+	fmt.Print(n)
+
+	// for i := 0; i < 100; i++ {
+	// 	fmt.Println(i)
+
+	// }
+}
