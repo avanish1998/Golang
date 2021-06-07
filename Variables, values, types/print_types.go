72aea9d89311b57a2efde0f17c875a22443bdf0d
Avanish Ghosh <avanishghosh@gmail.com>
2021-06-07 13:06:19 +0530

    topic-wise

---
+++ Variables, values, types/print_types.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var x = 22
+	fmt.Println(x)
+	fmt.Printf("%T\n", x) // prints type of x
+
+	var y = "hello"
+	fmt.Println(y)
+	fmt.Printf("%T\n", y) // prints type of y
+	//y = 43   --->  cannot use 43 (type untyped int) as type string in assignment
+
+}
